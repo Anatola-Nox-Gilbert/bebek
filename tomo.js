940eee7fe5e429cdd208d51d2cd2f7cbcf1f3628
Nox <anatolagilbert@gmail.com>
2025-08-27 13:31:51 +0200

    Tomo app main

---
+++ tomo.js
@@ -0,0 +1,101 @@
+
+const para = document.querySelector("p")
+
+let hungerDepletion = false
+const playBar = document.querySelector(".playBar")
+const sleepBar = document.querySelector(".sleepBar")
+const hungerBar = document.querySelector(".hungerBar")
+function enableStats(){
+    sleepBar.setAttribute("role", "statsProgress")
+    sleepBar.setAttribute("id", "sleepProgress")
+    sleepBar.setAttribute("aria-valuenow",100) // control over progress bar's value
+    sleepBar.style.setProperty('--progress', 100 + "%");
+    sleepBar.style.backgroundColor = " rgb(17, 58, 58)"
+
+    playBar.setAttribute("aria-valuenow",100)
+    playBar.setAttribute("id", "playProgress")
+    playBar.setAttribute("role","statsProgress")
+    playBar.style.setProperty('--progressPlay', 100 + "%");
+    playBar.style.backgroundColor = "rgb(17, 58, 58)"
+
+    hungerBar.setAttribute("aria-valuenow",100)
+    hungerBar.setAttribute("role", "statsProgress")
+    hungerBar.setAttribute("id", "hungerProgress")
+    hungerBar.style.setProperty('--progressHung', 100 + "%");
+    hungerBar.style.backgroundColor = "rgb(17, 58, 58)"
+    getSleepy()
+}
+// class progressbar = my sleepbar class - its const refers to that class
+enableStats();
+
+const sleepButton = document.getElementById("sleep")
+let sleepMinutes = 0;
+let lightOn = true;
+let count = 0;
+sleepButton.addEventListener("click", async (e) => {
+    if(count < 5){
+    count++
+    }
+    sleepBar.setAttribute("aria-valuenow", 20*count) // update aria-val 
+    sleepBar.style.setProperty('--progress', 20*count + "%");
+
+    if(lightOn == true){
+        //increase sleepMinutes by 4 for every minute
+    }
+    else{
+        //stop?
+    }
+})
+
+const playButton = document.getElementById("play")
+let playMinutes = 0
+let playCount = 0
+playButton.addEventListener( "click", async (e) =>{
+    if(playCount < 5){
+        playCount++
+    }
+    playBar.setAttribute("aria-valuenow", 20*playCount) // update aria-val 
+    playBar.style.setProperty('--progressPlay', 20*playCount + "%");
+})
+
+const hungerButton = document.getElementById("feeder")
+let hungMinutes = 0
+let hungCount = 0
+hungerButton.addEventListener( "click", async (e) =>{
+    if(hungCount < 5){
+        hungCount++
+    }
+    hungerBar.setAttribute("aria-valuenow", 20*hungCount) // update aria-val 
+    hungerBar.style.setProperty('--progressHung', 20*hungCount + "%");
+    if(!hungerDepletion){
+        hungerDepletion = true
+        setTimeout(getHungry,1800)
+    }
+    para.textContent = "called"
+})
+
+function getHungry(){
+    para.textContent = "check"
+    let temp = parseInt(hungerBar.getAttribute("aria-valuenow"))
+    hungerBar.setAttribute("aria-valuenow", temp-1) 
+    hungerBar.style.setProperty('--progressHung', temp-1 + "%")
+    if(temp > 10){
+        setTimeout(getHungry,1000)
+    }
+    else if(temp > 1){
+        setTimeout(getHungry,10000)
+    }
+    else{
+        hungerDepletion = false
+    }
+
+}
+
+function getSleepy(){
+    let temp = parseInt(sleepBar.getAttribute("aria-valuenow"))
+    if(temp > 0){
+        sleepBar.setAttribute("aria-valuenow", temp-1) 
+        sleepBar.style.setProperty('--progress', temp-1 + "%");
+        setTimeout(getSleep(28000))
+    }
+}
